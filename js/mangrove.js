f889112ab5856f3e5c8c0126b7b0be0cb3e15030
Simon Wells <siwells@gmail.com>
2018-04-16 23:49:14 +0100

    Revert "User can now add a node containing highlighted text in a tab, user can now import text to a tab form a .txt file, user can now export text from a tab to a .txt file, user can import a diagram in JSON format, user can now export diagram to JSON format, application will reload most recent state of diagram when it is opened, file management is done using filemanager.js"
    This reverts commit 8958d56e329d744d3c30a780f9169d8670075e20.

--- js/mangrove.js
+++ js/mangrove.js
@@ -1,4 +1,4 @@
-var cy = null;
+var cy;
 var mt = Mousetrap();
 var slideout;
 var cm = null;
@@ -6,128 +6,129 @@ var selected = [];
 var position = null;
 var layout = null;
 var running = false;
-var cyJson  = {};
-var xhttp = new XMLHttpRequest();
-var filename;
 
 var cola_params = {
     name: 'cola',
     animate: true,
     randomize: true,
     padding: 100,
-    fit: false,
+    fit: true,
     maxSimulationTime: 1500
 };
 
-initialise();
 
-function initialise() {
-	
-	//load diagram if there is one in localStorage
-	if (localStorage.getItem('state') != null)
-	{
-		loadJSON();
-		initCytoscape();
-	//else get from file
-	} else
-	{
-		json = "{\"nodes\": [{ \"data\": { \"id\": \"a1\", \"content\": \"Every person is going to die\",\"type\": \"atom\", \"typeshape\": \"roundrectangle\" },\"classes\": \"atom-label\" },{ \"data\": { \"id\": \"a2\", \"content\": \"You are a person\", \"type\": \"atom\", \"typeshape\": \"roundrectangle\" },\"classes\": \"atom-label\" },{ \"data\": { \"id\": \"a3\", \"content\": \"If you are going to die then you should treasure every moment\",\"type\": \"atom\", \"typeshape\": \"roundrectangle\" }, \"classes\": \"atom-label\" },{ \"data\": { \"id\": \"a4\", \"content\": \"You are going to die\", \"type\": \"atom\", \"typeshape\": \"roundrectangle\" },\"classes\": \"atom-label\" },{ \"data\": { \"id\": \"a5\", \"content\": \"You should treasure every moment\", \"type\": \"atom\", \"typeshape\": \"roundrectangle\" },\"classes\": \"atom-label\" },{ \"data\": { \"id\": \"s1\", \"content\": \"Default\\nSupport\", \"type\": \"scheme\", \"typeshape\": \"diamond\"  },\"classes\": \"scheme-label\" },{ \"data\": { \"id\": \"s2\", \"content\": \"Default\\nSupport\", \"type\": \"scheme\", \"typeshape\": \"diamond\"  },\"classes\": \"scheme-label\"}],\"edges\": [{ \"data\": { \"id\": \"a1s1\", \"source\": \"a1\", \"target\": \"s1\" } },{ \"data\": { \"id\": \"a2s1\", \"source\": \"a2\", \"target\": \"s1\" } },{ \"data\": { \"id\": \"a3s2\", \"source\": \"a3\", \"target\": \"s2\" } },{ \"data\": { \"id\": \"s2a5\", \"source\": \"s2\", \"target\": \"a5\" } },{ \"data\": { \"id\": \"s1a4\", \"source\": \"s1\", \"target\": \"a4\" } },{ \"data\": { \"id\": \"a4s2\", \"source\": \"a4\", \"target\": \"s2\" } }]}";
-		cyJson = JSON.parse(json);
-		initCytoscape();
-	}
-}
-
-function loadJSON() {
-	cyJson = JSON.parse(localStorage.getItem('state'));
-	if(cy != null)
-	{
-		cy.elements().remove();
-		cy.json({elements: cyJson});
-		redraw_visualisation();
-	}
-}
-
-function initCytoscape() {
 cy = cytoscape({
-	container: document.getElementById('cy'),
-	ready: function(){ window.cy = this; },
-	elements: cyJson ,
-	style:[
-		{ selector: 'node', style: { 
-			'content': 'data(content)', 
-			'text-opacity': 0.8, 
-			'width' : 'auto',
-			'height' : 'auto',
-			'text-valign': 'bottom', 
-			'text-halign': 'right',
-			}
-		},
-		{ selector: '[typeshape]', style: { 
-			'shape':'data(typeshape)'
-			}
-		},
+    container: document.getElementById('cy'),
+    ready: function(){ window.cy = this; },
+    elements:{
+        nodes: [
+        { data: { id: 'a1', content: 'Every person is going to die', 
+            type: 'atom', typeshape: 'roundrectangle' }, 
+            classes: 'atom-label' },
+        { data: { id: 'a2', content: 'You are a person', 
+            type: 'atom', typeshape: 'roundrectangle' },
+            classes: 'atom-label' },
+        { data: { id: 'a3', content: 'If you are going to die then you should treasure every moment',
+            type: 'atom', typeshape: 'roundrectangle' }, 
+            classes: 'atom-label' },
+        { data: { id: 'a4', content: 'You are going to die', 
+            type: 'atom', typeshape: 'roundrectangle' }, 
+            classes: 'atom-label' },
+        { data: { id: 'a5', content: 'You should treasure every moment', 
+            type: 'atom', typeshape: 'roundrectangle' },
+            classes: 'atom-label' },
 
-		{ selector: 'edge', style: { 
-			'line-color': '#9dbaea',
-			'target-arrow-shape': 'triangle',
-			'target-arrow-color': '#9dbaea', 
-			'curve-style': 'bezier' 
-			}
-		},
-		{ selector: ':selected', style: {
-			'border-width':'3',
-			'border-color':'#333333'
-			}
-		},
-		{
-			selector: '.atom-label', style:{
-				'text-wrap': 'wrap',
-				'text-max-width': 80,
-			}
-		},
-		{
-			selector: '.scheme-label', style:{
-				'text-wrap': 'wrap',
-			}
-		}                    
-		],
-		boxSelectionEnabled: false,
-		autounselectify: false,
-		selectionType: 'single',
-		minZoom: 0.05,
-		maxZoom: 2
+        { data: { id: 's1', content: 'Default\nSupport', type: 'scheme', typeshape: 'diamond'  },
+            classes: 'scheme-label' },
+        { data: { id: 's2', content: 'Default\nSupport', type: 'scheme', typeshape: 'diamond'  },
+            classes: 'scheme-label'},
+        ],
+        edges: [
 
-});
-	
-	layout = build_cola_layout();
-	layout.run();
+        { data: { id: 'a1s1', source: 'a1', target: 's1' } },
+        { data: { id: 'a2s1', source: 'a2', target: 's1' } },
+        { data: { id: 'a3s2', source: 'a3', target: 's2' } },
+        { data: { id: 's2a5', source: 's2', target: 'a5' } },
+        { data: { id: 's1a4', source: 's1', target: 'a4' } },
+        { data: { id: 'a4s2', source: 'a4', target: 's2' } },
+        ]
+        },
+    style:[
+        { selector: 'node', style: { 
+            'content': 'data(content)', 
+            'text-opacity': 0.8, 
+            'width' : 'auto',
+            'height' : 'auto',
+            'text-valign': 'bottom', 
+            'text-halign': 'right',
+            }
+        },
+        { selector: '[typeshape]', style: { 
+            'shape':'data(typeshape)'
+            }
+        },
 
-	cy.elements('node[type = "atom"]').qtip({
-		content: 'Metadata about this atom',
-		position: {
-			my: 'top center',
-			at: 'bottom center'
-		},
-		style: {
-			classes: 'qtip-bootstrap',
-			tip: {
-				width: 16,
-				height: 8
-			}
-		}
-	});
+        { selector: 'edge', style: { 
+            'line-color': '#9dbaea',
+            'target-arrow-shape': 'triangle',
+            'target-arrow-color': '#9dbaea', 
+            'curve-style': 'bezier' 
+            }
+        },
+        { selector: ':selected', style: {
+            'border-width':'3',
+            'border-color':'#333333'
+            }
+        },
+        {
+            selector: '.atom-label', style:{
+                'text-wrap': 'wrap',
+                'text-max-width': 80,
+            }
+        },
+        {
+            selector: '.scheme-label', style:{
+                'text-wrap': 'wrap',
+            }
+        }                    
+        ],
+        boxSelectionEnabled: false,
+        autounselectify: false,
+        selectionType: 'single',
+        minZoom: 0.05,
+        maxZoom: 2
+
+    });
+
+    layout = build_cola_layout();
+    layout.run();
+
+    cy.elements('node[type = "atom"]').qtip({
+        content: 'Metadata about this atom',
+        position: {
+            my: 'top center',
+            at: 'bottom center'
+        },
+        style: {
+            classes: 'qtip-bootstrap',
+            tip: {
+                width: 16,
+                height: 8
+            }
+        }
+    });
 
 
    cy.edgehandles({
-		toggleOffOnLeave: true,
+	    toggleOffOnLeave: true,
 		handleNodes: "node",
 		handleSize: 20,
-		handleColor: 'orange',
-		handleHitThreshold: 8,
-		handleLineWidth: 5,
-		//handleLineType: 'flat',
-		handleOutlineColor: 'grey',
-		edgeType: function(){ return 'flat'; }
+        handleColor: 'orange',
+        handleHitThreshold: 8,
+        handleLineWidth: 5,
+        //handleLineType: 'flat',
+        handleOutlineColor: 'grey',
+        edgeType: function(){ return 'flat'; }
 	});
 
 /*
@@ -174,7 +175,7 @@ cm = cy.contextMenus({
         onClickFunction: function (event) {
           var target = event.target || event.cyTarget;
           removed = target.remove();
-          localStorage.setItem("state","{\"nodes\":"+JSON.stringify(cy.elements().jsons())+"}");
+          
           cm.showMenuItem('undo-last-remove');
         },
         hasTrailingDivider: true
@@ -202,14 +203,7 @@ cm = cy.contextMenus({
             position = event.position || event.cyPosition;
             
            // document.getElementById("new_atom_content").options.selectedIndex=0;
-            var selected_text = get_selected_text();
-			if (selected_text == undefined)
-			{
-				$('#newAtomModal').modal('show');
-			} else {
-				console.log(selected_text);
-				add_new_atom_node_with_text(selected_text);
-			}
+            $('#newAtomModal').modal('show');
         }
       },
       {
@@ -264,7 +258,7 @@ cm = cy.contextMenus({
             cy.resize(); 
         }
     });
-}
+
 
 /*
  *
@@ -276,38 +270,11 @@ cm = cy.contextMenus({
         var new_content = document.getElementById("new_atom_content").value; 
         cy.add([
             {group: "nodes", data: {id: Math.floor(Math.random() * 1024).toString(), 
-                content: new_content, type: 'atom', typeshape: 'roundrectangle' }, classes: 'atom-label', locked: false, renderedPosition: position},
+                content: new_content, typeshape: 'roundrectangle' }, classes: 'atom-label', locked: false, renderedPosition: position},
         ]);
-        localStorage.setItem("state","{\"nodes\":"+JSON.stringify(cy.elements().jsons())+"}");
+        redraw_visualisation();
         console.log( cy.elements().jsons() );
-
     }
-	
-	//******************************************************
-	function add_new_atom_node_with_text(selected_text) {
-		cy.add([
-            {group: "nodes", data: {id: Math.floor(Math.random() * 1024).toString(), 
-                content: selected_text, type: 'atom', typeshape: 'roundrectangle' }, classes: 'atom-label', locked: false, renderedPosition: position},
-        ]);
-		localStorage.setItem("state","{\"nodes\":"+JSON.stringify(cy.elements().jsons())+"}");
-        console.log( cy.elements().jsons() );
-		selected_text = undefined;
-		window.getSelection().removeAllRanges()
-	}
-	
-	function get_selected_text() {
-		var selected_text;
-		if (window.getSelection().toString().length>0)
-		{
-			if(window.getSelection().baseNode.id=="textarea")
-			{
-				selected_text = window.getSelection().toString();
-				console.log("'" + selected_text + "' selected")
-				return selected_text;
-			}
-		}
-	}
-	//******************************************************
 
     function add_new_scheme_node() {
         var scheme_idx = document.getElementById("sel1").options.selectedIndex;
@@ -316,20 +283,25 @@ cm = cy.contextMenus({
             {group: "nodes", data: {id: Math.floor(Math.random() * 1024).toString(), 
                 content: scheme, typeshape: 'diamond' }, classes: 'scheme-label', locked: false, renderedPosition: position},
         ]);
-		localStorage.setItem("state","{\"nodes\":"+JSON.stringify(cy.elements().jsons())+"}");
     };
 
     function delete_nodes() {    
         cy.remove( cy.getElementById(selected[0]) );
         selected = []
-		redraw_visualisation();
+    };
+
+    function download(text, name, type) {
+        console.log("DL");
+        var a = document.getElementById("export_button");
+        var file = new Blob([text], {type: type});
+        a.href = URL.createObjectURL(file);
+        a.download = name;
     };
 
     function redraw_visualisation() {
         layout.stop();
         layout.options.eles = cy.elements();
         layout.run();
-
     };
 
 
@@ -374,7 +346,6 @@ function build_cola_layout( opts )
  * Mousetrap - keypboard handler functions
  *
  * */
- //TODO: ADD FUNCTIONS
 
 mt.bind('a', function() {
     console.log("ADD NODE OR EDGE");
