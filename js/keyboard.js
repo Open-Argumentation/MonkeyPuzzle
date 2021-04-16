49f5611b079b973aa3f381d750d05012c72823d4
Simon Wells <siwells@gmail.com>
2021-04-16 11:28:43 +0100

    Remove unused shortcuts

--- js/keyboard.js
+++ js/keyboard.js
@@ -17,12 +17,10 @@ mt.bind("d", function() {
     selected = [];
 });
 
-//mt.bind("f", function() { console.log("FIX NODE PLACEMENT"); });
 mt.bind("h", function() { $("#help_modal").modal("show"); });
 mt.bind("m", function() { toggle_menu(); });
 mt.bind("r", function() { toggle_resource_pane(); });
-//mt.bind("s", function() { console.log("SCALE SELECTED NODE"); });
+mt.bind("s", function() { $("#newSchemeModal").modal("show"); });
 mt.bind("t", function() { $("#resource_pane_selection_modal").modal("show"); });
 mt.bind("c", function() { if(selected.length>1) {$("#newCompoundModal").modal("show"); }});
-//mt.bind(["command+z","ctrl+z"], function() { undo(); });
-//mt.bind(["command+y","ctrl+y"], function() { redo(); });
+
